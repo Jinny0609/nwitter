c0161e1d78d30cd421b632da8a1a0f20bd22e3e9
Jinny0609 <wlsghwns1@naver.com>
2022-11-04 20:48:51 +0900

    chapter2

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1yB5lqhuCc-1fJ23_4n4EBmcTmq6R-ek",
+  authDomain: "nwitter-609b8.firebaseapp.com",
+  projectId: "nwitter-609b8",
+  storageBucket: "nwitter-609b8.appspot.com",
+  messagingSenderId: "917307167833",
+  appId: "1:917307167833:web:8eff4d0efc431010a10d48"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
