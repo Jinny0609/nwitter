7d734da5d8e4109ef5474c5981cdeb7f60e5f992
Jinny0609 <wlsghwns1@naver.com>
2022-11-05 21:09:11 +0900

    chapter5

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +20,4 @@ firebase.initializeApp(firebaseConfig);
 // Initialize Firebase
 export const firebaseInstance = firebase;
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
