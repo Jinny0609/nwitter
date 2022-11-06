be8bb172363aed722cb412741abedf1d6cd65e19
Jinny0609 <wlsghwns1@naver.com>
2022-11-06 22:02:38 +0900

    chapter6

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,57 +1,61 @@
-import { dbService } from "fbase";
+import { dbService, storageService } from "fbase";
 import { useState } from "react";
 
-const Nweet = ({ nweetObj, isOwner}) => {
-    const [editing, setEditing] = useState(false);
-    const [newNweet, setNewNweet] = useState(nweetObj.text);
-    const onDeleteClick = async () => {
-        const ok = window.confirm("삭제하시겠습니까?");
-        console.log(ok);
-        if(ok){
-            console.log(nweetObj.id);
-            const data = await dbService.doc(`nweets/${nweetObj.id}`).delete();
-            console.log(data);
-        }
-    };
+const Nweet = ({ nweetObj, isOwner }) => {
+  const [editing, setEditing] = useState(false);
+  const [newNweet, setNewNweet] = useState(nweetObj.text);
 
-    const toggleEditing = () => setEditing((prev) => !prev);
+  const onDeleteClick = async () => {
+    const ok = window.confirm("삭제하시겠습니까?");
 
-    const onChange = (event) => {
-        const{
-            target: { value },
-        } = event;
-        setNewNweet(value);
-    };
+    if (ok) {
+      await dbService.doc(`nweets/${nweetObj.id}`).delete();
+      if (nweetObj.attachmentUrl !== "")
+        await storageService.refFromURL(nweetObj.attachmentUrl).delete();
+    }
+  };
 
-    const onSubmit = async (event) =>{
-        event.preventDefault();
-        await dbService.doc(`nweets/${nweetObj.id}`).update({text: newNweet});
-        setEditing(false);
-    };
+  const toggleEditing = () => setEditing((prev) => !prev);
 
-    return(
-        <div>
-            {editing ? (
-                <>
-                    <form onSubmit={onSubmit}>
-                        <input onChange={onChange} value={newNweet} required />
-                        <input type="submit" value="Update Nweet" />
-                    </form>
-                    <button onClick={toggleEditing}>Cancel</button>
-                </>
-            ) : (
-                <>
-                    <h4>{nweetObj.text}</h4>
-                    {isOwner && (
-                        <>
-                            <button onClick={onDeleteClick}>Delete Nweet</button>
-                            <button onClick={toggleEditing}>Edit Nweet</button>
-                        </>
-                    )}
-                </>
-            )}
-        </div>
-    );
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewNweet(value);
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.doc(`nweets/${nweetObj.id}`).update({ text: newNweet });
+    setEditing(false);
+  };
+
+  return (
+    <div>
+      {editing ? (
+        <>
+          <form onSubmit={onSubmit}>
+            <input onChange={onChange} value={newNweet} required />
+            <input type="submit" value="Update Nweet" />
+          </form>
+          <button onClick={toggleEditing}>Cancel</button>
+        </>
+      ) : (
+        <>
+          <h4>{nweetObj.text}</h4>
+          {nweetObj.attachmentUrl && (
+            <img src={nweetObj.attachmentUrl} width="50px" height="50px" />
+          )}
+          {isOwner && (
+            <>
+              <button onClick={onDeleteClick}>Delete Nweet</button>
+              <button onClick={toggleEditing}>Edit Nweet</button>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Nweet;
