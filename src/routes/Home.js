82dccd7a543b128ae08f6634cdaa99133f6617f3
Jinny0609 <wlsghwns1@naver.com>
2022-11-09 13:39:26 +0900

    nwitter

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,7 +6,10 @@ import NweetFactory from "components/NweetFactory";
 const Home = ({ userObj }) => {
   const [nweets, setNweets] = useState([]);
   useEffect(() => {
-    dbService.collection("nweets").onSnapshot((snapshot) => {
+    dbService
+    .collection("nweets")
+    .orderBy("createdAt", "desc")
+    .onSnapshot((snapshot) => {
       const newArray = snapshot.docs.map((document) => ({
         id: document.id,
         ...document.data(),
@@ -16,9 +19,9 @@ const Home = ({ userObj }) => {
   }, []);
 
   return (
-    <>
+    <div className="container">
       <NweetFactory userObj={userObj} />
-      <div>
+      <div style={{marginTop: 30}}>
         {nweets.map((nweet) => (
           <Nweet
             key={nweet.id}
@@ -27,7 +30,7 @@ const Home = ({ userObj }) => {
           />
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
